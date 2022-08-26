ea0c9d07ccc2d85a26cf1e1bc631097b197ade56
Somelina Obiechina <ifeasome@gmail.com>
2022-08-25 20:11:18 -0500

    Adds Swagger documentation to project

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const routes = require('./controllers');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const swaggerDocument = YAML.load('./swagger.yml');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // turn on connection to db and server
 module.exports = app.listen(PORT, () => {
